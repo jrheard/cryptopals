1535704e35626428595cceb6f44fba0ab73ae2fc
jrheard <jrheard@cs.stanford.edu>
2017-12-23 10:41:35 -0800

    WIP on 1.3

--- src/cryptopals/core.clj
+++ src/cryptopals/core.clj
@@ -1,4 +1,6 @@
 (ns cryptopals.core
+  (:require [clojure.string :refer [trim]]
+            [com.rpl.specter :refer [select transform ALL MAP-VALS]])
   (:import java.util.Base64))
 
 ; from https://stackoverflow.com/questions/10062967/clojures-equivalent-to-pythons-encodehex-and-decodehex
@@ -33,20 +35,30 @@
   [byte-array-1 byte-array-2]
   (byte-array (map bit-xor byte-array-1 byte-array-2)))
 
+(defn score-string
+  "Returns a number between 0.0 and 1.0, indicating how likely it is that `s` is some English text."
+  [s]
+  (let [letters (select [ALL #(Character/isLetter %)] s)]
+    (/ (count letters) (count s))
+    )
+  )
 
 (comment
 
   (let [foo (unhexify "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736")]
     (for [character (map byte "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")]
-      (let [character-buffer (byte-array (repeat (count foo) character))]
-        (bytes->str
-          (fixed-xor foo character-buffer)
-          )
-        )
+      (let [character-buffer (byte-array (repeat (count foo) character))
+            decoded (bytes->str (fixed-xor foo character-buffer))]
+        [(char character)
+         decoded
+         (score-string decoded)])))
 
+  (Integer/toString (int \X) 2)
+  (int \X)
 
-      )
-
+  (let [foo "Cooking MC's like a pound of bacon"
+        bar (frequencies foo)]
+    (score-string foo)
     )
 
   )
