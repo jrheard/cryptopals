24a04985e0903c325c5c25455d416f133fd6e873
jrheard <jrheard@cs.stanford.edu>
2017-12-23 07:54:18 -0800

    1.1

--- project.clj
+++ project.clj
@@ -3,7 +3,12 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.9.0"]
+
+                 [com.rpl/specter "1.0.5"]
+
+                 [org.clojure/spec.alpha "0.1.143"]
+                 [orchestra "2017.11.12-1"]]
   :main ^:skip-aot cryptopals.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/cryptopals/core.clj
+++ src/cryptopals/core.clj
@@ -1,7 +1,33 @@
 (ns cryptopals.core
-  (:gen-class))
+  (:import java.util.Base64))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+; from https://stackoverflow.com/questions/10062967/clojures-equivalent-to-pythons-encodehex-and-decodehex
+(defn unhexify "Convert hex string to byte sequence" [s]
+  (letfn [(unhexify-2 [c1 c2]
+            (unchecked-byte
+              (+ (bit-shift-left (Character/digit c1 16) 4)
+                 (Character/digit c2 16))))]
+    (->> (partition 2 s)
+         (map #(apply unhexify-2 %))
+         (map byte)
+         byte-array)))
+
+; "Cryptopals Rule:
+; Always operate on raw bytes, never on encoded strings. Only use hex and base64 for pretty-printing."
+
+(defn hex->base64
+  [hex]
+  (->> hex
+       unhexify
+       (.encodeToString (Base64/getEncoder))))
+
+
+(comment
+  (let [foo "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"]
+    (=
+      (hex->base64 foo)
+      "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t")
+
+    )
+
+  )
