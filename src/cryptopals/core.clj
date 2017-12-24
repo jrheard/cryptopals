00f21e6dbcb803a182a5b754f0f88d3f5a021201
jrheard <jrheard@cs.stanford.edu>
2017-12-24 10:24:10 -0600

    progress on a way better scoring function, still not perfect but way better

--- src/cryptopals/core.clj
+++ src/cryptopals/core.clj
@@ -1,6 +1,6 @@
 (ns cryptopals.core
-  (:require [clojure.string :refer [trim]]
-            [com.rpl.specter :refer [select transform ALL MAP-VALS]])
+  (:require [clojure.string :refer [trim lower-case]]
+            [com.rpl.specter :refer [select transform ALL MAP-VALS MAP-KEYS]])
   (:import java.util.Base64))
 
 ; from https://stackoverflow.com/questions/10062967/clojures-equivalent-to-pythons-encodehex-and-decodehex
@@ -35,46 +35,84 @@
   [byte-array-1 byte-array-2]
   (byte-array (map bit-xor byte-array-1 byte-array-2)))
 
+(def ENGLISH-CHARACTER-FREQUENCIES
+  {\a 0.0855
+   \b 0.016
+   \c 0.0316
+   \d 0.0387
+   \e 0.121
+   \f 0.0218
+   \g 0.0209
+   \h 0.0496
+   \i 0.0733
+   \j 0.0022
+   \k 0.0081
+   \l 0.0421
+   \m 0.0253
+   \n 0.0717
+   \o 0.0747
+   \p 0.0207
+   \q 0.001
+   \r 0.0633
+   \s 0.0673
+   \t 0.0894
+   \u 0.0268
+   \v 0.0106
+   \w 0.0183
+   \x 0.0019
+   \y 0.0172
+   \z 0.0011})
+
+(defn chi-squared-score
+  [expected observed]
+  ; TODO - ignore things that don't appear in expected?
+
+  (apply +
+         (for [[character expected-occurrences] expected]
+           (do
+             (/ (Math/pow (- expected-occurrences
+                             (observed character 0))
+                          2)
+                expected-occurrences)))))
+
 (defn score-string
-  "Returns a number between 0.0 and 1.0, indicating how likely it is that `s` is some English text."
+  "Returns a number, 0 or greater, indicating how likely it is that `s` is some English text. Lower is better."
   [s]
-  (let [letters (filter #(Character/isLetter %) s)
-        lowercase-letters (filter #(Character/isLowerCase %) s)
-        digits (filter #(Character/isDigit %) s)
-        normal-sentence-symbols-re #"[!@., ;:\(\)'\"]"
-        normal-sentence-symbols (re-seq normal-sentence-symbols-re s)
+  (let [expected (transform [MAP-VALS] #(* % (count s)) ENGLISH-CHARACTER-FREQUENCIES)
+        observed (transform [MAP-KEYS] #(first (seq (lower-case %))) (frequencies s))]
+    (chi-squared-score expected observed)
 
-        everything-else (filter #(not
-                                   (or
-                                     (Character/isLetter %)
-                                     (Character/isDigit %)
-                                     (re-seq normal-sentence-symbols-re (str %))))
-                                s)
+    )
 
-        scores [(- 0.8
-                   (/ (count letters) (count s)))
-                (- 0.9
-                   (/ (count lowercase-letters) (count letters)))
-                (- 0.2
-                   (/ (+ (count digits) (count normal-sentence-symbols))
-                      (count s)))
-                (/ (count everything-else) (count s))]]
-
-    (- 1
-       (/ (apply +
-                 (for [score scores]
-                   (min (Math/abs (float score)) 1)))
-          (count scores)))))
+  )
 
 (comment
+  (score-string "JOIFWEJOIFEW")
 
-  (let [foo (unhexify "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736")]
-    (for [character (map byte "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")]
-      (let [character-buffer (byte-array (repeat (count foo) character))
-            decoded (bytes->str (fixed-xor foo character-buffer))]
-        [(char character)
-         decoded
-         (score-string decoded)])))
+  (frequencies "JIOFEWJIOFEW")
+
+  (seq "j")
+
+
+
+
+  (let [foo "Cooking MC's like a pound of bacon"]
+    (score-string foo)
+
+    )
+
+
+  (score-string "Uyy}xq6[U1e6z}s6w6fycxr6yp6twuyx")
+
+  (sort-by
+    #(nth % 2)
+    (let [foo (unhexify "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736")]
+     (for [character (map byte "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")]
+       (let [character-buffer (byte-array (repeat (count foo) character))
+             decoded (bytes->str (fixed-xor foo character-buffer))]
+         [(char character)
+          decoded
+          (score-string decoded)]))))
 
   (Integer/toString (int \X) 2)
   (int \X)
