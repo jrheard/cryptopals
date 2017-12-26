28a3e6e9b14e0e3169518525f5d3e7eba01a932b
jrheard <jrheard@cs.stanford.edu>
2017-12-26 17:35:50 -0600

    challenge 1.5

--- src/cryptopals/core.clj
+++ src/cryptopals/core.clj
@@ -114,11 +114,27 @@
                      #(nth % 2)
                      valid-attempts)))))
 
+(defn repeating-key-xor-encrypt
+  [plaintext-bytes key-bytes]
+  (map bit-xor plaintext-bytes (cycle key-bytes)))
+
 
 (comment
+  "b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272
+  a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f"
+
+
+  (let [foo "Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal"]
+    (take 5 (unhexify "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f"))
+    (take 5 (repeating-key-xor-encrypt (.getBytes foo) (.getBytes "ICE")))
+    #_(hexify (repeating-key-xor-encrypt (.getBytes foo)
+                                (.getBytes "ICE"))))
+
+
+  (take 10 (cycle (.getBytes "ICE")))
+
   (score-string "Cooking MC's like a pound of bacon")
   (score-string "Now that the party is jumping\n")
   (score-string "|t,W3\"8cOUbdHÉVIv{S0?6ÏeBlp")
-
 
   )
--- test/cryptopals/core_test.clj
+++ test/cryptopals/core_test.clj
@@ -22,3 +22,8 @@
            (first (filter identity (map #(detect-single-character-xor (unhexify %))
                                         inputs))))
          "Now that the party is jumping\n")))
+
+(deftest set-1-challenge-5
+  (is (= (repeating-key-xor-encrypt (.getBytes "Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal")
+                                    (.getBytes "ICE"))
+         (unhexify "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f"))))
