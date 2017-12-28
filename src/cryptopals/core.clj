324c847c127fdf35b3cf3fdaed48a076f5bdd04a
jrheard <jrheard@cs.stanford.edu>
2017-12-28 13:36:18 -0800

    implement discover-cipher-block-size

--- src/cryptopals/core.clj
+++ src/cryptopals/core.clj
@@ -264,8 +264,8 @@
   ; "Write a function to generate a random AES key; that's just 16 random bytes."
   (byte-array (take 16 (repeatedly #(rand-int 256)))))
 
-; Write a function that encrypts data under an unknown key --- that is,
-; a function that generates a random key and encrypts under it.
+; "Write a function that encrypts data under an unknown key --- that is,
+; a function that generates a random key and encrypts under it."
 (defn aes-encrypt-with-random-key-and-padding
   [bytes]
   (let [key (generate-aes-key)
@@ -279,25 +279,36 @@
       (aes-cbc-encrypt padded-bytes key (generate-aes-key)))))
 
 (defn ciphertext-likely-encrypted-with-ecb-mode?
-  ; XXXX NOT RELIABLE!!!!!
   [ciphertext-bytes]
   (let [chunks (partition 16 ciphertext-bytes)
         dupe-chunks (duplicates chunks)]
     (> (count dupe-chunks) 0)))
 
-; Copy your oracle function to a new function that encrypts buffers under ECB mode using a consistent but unknown key (for instance, assign a single random key, once, to a global variable).
-; Now take that same function and have it append to the plaintext, BEFORE ENCRYPTING, the following string:
+(defn discover-cipher-block-size
+  [cipher-encrypt-fn]
+  (loop [i 1
+         last-seen-ciphertext-size nil]
+    (let [ciphertext-size (cipher-encrypt-fn (.getBytes (apply str (repeat i "A"))))]
+
+      (if (and last-seen-ciphertext-size
+               (not= ciphertext-size last-seen-ciphertext-size))
+
+        last-seen-ciphertext-size
+        (recur (inc i) (count ciphertext-size))))))
+
+; TODO make "does-cipher-encryption-use-ecb-mode?" function
 
 (comment
+
+  (.getBytes (apply str (repeat 5 "a")))
 
 
   (let [key (generate-aes-key)
         bytes-to-append (base64->bytes "Um9sbGluJyBpbiBteSA1LjAKV2l0aCBteSByYWctdG9wIGRvd24gc28gbXkgaGFpciBjYW4gYmxvdwpUaGUgZ2lybGllcyBvbiBzdGFuZGJ5IHdhdmluZyBqdXN0IHRvIHNheSBoaQpEaWQgeW91IHN0b3A/IE5vLCBJIGp1c3QgZHJvdmUgYnkK")
-        plaintext-bytes (.getBytes "I'm back and I'm ringin' the bell \nA rockin' on the mike while the fly girls yell \nIn ecstasy in the back of me \nWell that's my DJ Deshay cuttin' all them Z's \nHittin' hard and the girlies goin' crazy \nVanilla's on the mike, man I'm not lazy. \n\nI'm lettin' my drug kick in \nIt controls my mouth and I begin \nTo just let it flow, let my concepts go \nMy posse's to the side yellin', Go Vanilla Go! \n\nSmooth 'cause that's the way I will be \nAnd if you don't give a damn, then \nWhy you starin' at me \nSo get off 'cause I control the stage \nThere's no dissin' allowed \nI'm in my own phase \nThe girlies sa y they love me and that is ok \nAnd I can dance better than any kid n' play \n\nStage 2 -- Yea the one ya' wanna listen to \nIt's off my head so let the beat play through \nSo I can funk it up and make it sound good \n1-2-3 Yo -- Knock on some wood \nFor good luck, I like my rhymes atrocious \nSupercalafragilisticexpialidocious \nI'm an effect and that you can bet \nI can take a fly girl and make her wet. \n\nI'm like Samson -- Samson to Delilah \nThere's no denyin', You can try to hang \nBut you'll keep tryin' to get my style \nOver and over, practice makes perfect \nBut not if you're a loafer. \n\nYou'll get nowhere, no place, no time, no girls \nSoon -- Oh my God, homebody, you probably eat \nSpaghetti with a spoon! Come on and say it! \n\nVIP. Vanilla Ice yep, yep, I'm comin' hard like a rhino \nIntoxicating so you stagger like a wino \nSo punks stop trying and girl stop cryin' \nVanilla Ice is sellin' and you people are buyin' \n'Cause why the freaks are jockin' like Crazy Glue \nMovin' and groovin' trying to sing along \nAll through the ghetto groovin' this here song \nNow you're amazed by the VIP posse. \n\nSteppin' so hard like a German Nazi \nStartled by the bases hittin' ground \nThere's no trippin' on mine, I'm just gettin' down \nSparkamatic, I'm hangin' tight like a fanatic \nYou trapped me once and I thought that \nYou might have it \nSo step down and lend me your ear \n'89 in my time! You, '90 is my year. \n\nYou're weakenin' fast, YO! and I can tell it \nYour body's gettin' hot, so, so I can smell it \nSo don't be mad and don't be sad \n'Cause the lyrics belong to ICE, You can call me Dad \nYou're pitchin' a fit, so step back and endure \nLet the witch doctor, Ice, do the dance to cure \nSo come up close and don't be square \nYou wanna battle me -- Anytime, anywhere \n\nYou thought that I was weak, Boy, you're dead wrong \nSo come on, everybody and sing this song \n\nSay -- Play that funky music Say, go white boy, go white boy go \nplay that funky music Go white boy, go white boy, go \nLay down and boogie and play that funky music till you die. \n\nPlay that funky music Come on, Come on, let me hear \nPlay that funky music white boy you say it, say it \nPlay that funky music A little louder now \nPlay that funky music, white boy Come on, Come on, Come on \nPlay that funky music \n")
+        plaintext-bytes (.getBytes (apply str (repeat 300 "A")))
         plaintext-bytes (concat plaintext-bytes bytes-to-append)]
 
-
-
+    (discover-cipher-block-size #(aes-ecb-encrypt (pkcs7-pad % 16) key))
     )
 
   )
